813497858b36064388290a97106442cc1d11740d
Danny Ngo <danny.ngo9@gmail.com>
2016-02-29 20:50:48 +0000

    snake game final

--- game.js
+++ game.js
@@ -1,39 +1,162 @@
-var gamePiece;
-
-function startGame(){
-	gamePiece = new components(30,30,"red",10,120);
-	myGame.start();
+$(document).ready(function(){
 	
-}
+	//get canvas size
+	var canvas = $('#canvas')[0];
+	var ctx = canvas.getContext("2d");
+	var w = $("#canvas").width();
+	var h = $("#canvas").height();
 
-var myGame = {
-	canvas : document.createElement("canvas"),
-	start : function (){
-		this.canvas.width = 480;
-		this.canvas.height = 270;
-		this.context = this.canvas.getContext('2d');
-		document.body.insertBefore(this.canvas,document.body.childNodes[0]);
-		this.interval = setInterval(updateGameArea,20);
-	},
-	clear : function(){
-		this.context.clearRect(0,0,this.canvas.width,this.canvas.height)
-	}
-}
-
-function components (width,height,colour,x,y){
-	this.x = x;
-	this.y = y;
-	this.width = width;
-	this.height = height;
-	this.update = function(){
-	cxt = myGame.context;
-	cxt.fillStyle = colour;
-	cxt.fillRect(this.x,this.y,this.width,this.height);
-	}
-}
-function updateGameArea(){
-	myGame.clear();
-	gamePiece.x +=1;
-	gamePiece.update();
+	//cell width
+	var cw = 10;
 	
-}
+	var direction;
+
+	var food;
+
+	var score = 0;
+
+	var snake_array; // an array of cells to make up snake
+	
+
+	
+
+	function init(){
+		direction = "right";
+		createSnake()
+		createFood();
+		
+		
+		if(typeof game_loop !="undefined") clearInterval(game_loop);
+
+		game_loop = setInterval(paint,60);
+
+	}
+
+	init();
+
+	
+
+	
+	
+	function createSnake (){
+
+		var lenght = 5;
+		snake_array = [];
+		for(var i = lenght-1; i >= 0 ;i--){
+			//create the snake 
+			//put x and y co 
+			snake_array.push({x:i,y:0})
+		}
+	}
+
+	//paint the snake
+	function paint(){
+
+		//draw canvas borders 
+	ctx.fillStyle = "white";
+	ctx.fillRect(0,0,w,h);
+	ctx.strokeStyle = "black";
+	ctx.strokeRect(0,0,w,h);
+
+		//movement code for the snake
+
+		
+
+		var tx = snake_array[0].x;
+		var ty = snake_array[0].y; 
+
+		if(direction=="left"){
+			tx++;
+		}
+		else if(direction=="right"){
+			tx--;
+		}
+		else if(direction=="down"){
+			ty++;
+		}
+		else if (direction=="up"){
+			ty--;
+		}
+
+
+		if(tx == -1 || tx == w/cw || ty == -1 || ty == h/cw || checkCollision(tx, ty, snake_array)){
+			init();
+			return;
+		}
+
+		//create head instead of moiving tail
+
+		if(tx == food.x && ty == food.y){
+		var tail = {x:tx,y:ty};
+		createFood();
+		score++;
+		}
+		else{
+			var tail = snake_array.pop();
+			tail.x = tx;
+			tail.y = ty;
+		}
+
+		
+		//console.log(tail);
+		snake_array.unshift(tail);
+
+		for(var i = 0; i < snake_array.length; i++){
+			var c = snake_array[i];
+
+			paintCell(c.x, c.y);
+			
+		}
+
+		paintCell(food.x, food.y);
+		var drawScore = "Score: " + score;
+		ctx.fillText(drawScore,5,h-5);
+	}
+
+	function paintCell(x,y){
+		ctx.fillStyle = "blue";
+		ctx.fillRect(x,y,cw,cw);
+		ctx.strokeStyle = "white";
+		ctx.strokeRect(x,y,cw,cw);
+	}
+
+	function createFood(){
+
+		 food = {
+			x:Math.round(Math.random()*(w-cw)/cw),
+			y:Math.round(Math.random()*(h-cw)/cw)
+		};
+	}
+
+	function checkCollision(x,y,array){
+		for(i=0;i<array.length;i++){
+			if(array[i].x==x && array[i].y==y)
+				return true;
+		}
+		return false;
+	}
+
+	$(document).keydown(function(e){
+
+		var key = e.which; 
+		console.log(key);
+		if(key == "37" && direction != "left"){
+			direction = "right";
+		}
+		else if(key == "38" && direction !="down"){
+			direction = "up";
+		}
+		else if(key == "39" && direction !="left"){
+			direction = "right";
+		}
+		else if(key == "40" && direction !="up"){
+			direction = "down";
+		}
+	})
+
+	
+	
+	
+
+
+});
